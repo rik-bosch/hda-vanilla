5cc45e155859e051dd2414e4a45689edddea5bce
Rik Bosch <rik.bosch@essent.nl>
2024-05-05 15:41:57 +0200

    Extra features

---
+++ README.md
@@ -0,0 +1,15 @@
+# hda-vanilla
+
+hda-vanilla is a demonstration of the basic concepts of Hypermedia-Driven Applications, using only vanilla HTML/CSS/JS. It also uses a barebones PHP server, to mimick a single REST endpoint.
+
+## Prerequisites
+
+This application needs PHP version 8+.
+
+## Usage
+
+To run the application, run a PHP dev server from the public folder:
+
+	cd public
+	php -S localhost:8080
+
--- src/UserIndex.php
+++ src/UserIndex.php
@@ -4,23 +4,42 @@ class UserIndex {
     private array $data;
     private int $offset = 0;
     private int $limit = 20;
+    private ?string $sortBy = null;
 
     function __construct(array $data, array $params) {
-        $this->data = $data;
         if (array_key_exists('offset', $params)) {
             $this->offset = intval($params['offset']);
         }
         if (array_key_exists('limit', $params)) {
             $this->limit = intval($params['limit']);
         }
+        if (array_key_exists('sortBy', $params)) {
+            $key = $params['sortBy'];
+            if ($key !== 'id' && array_key_exists($key, $data[0])) {
+                $this->sortBy = $key;
+                usort($data, function($a,$b) use ($key) { return strcmp($a[$key], $b[$key]); });
+            }
+        }
+        $this->data = $data;
     }
 
     public function getData(): array {
         return array_slice($this->data, $this->offset, $this->limit);
     }
 
+    public function getOffset(): int {
+        return $this->offset;
+    }
+
+    public function getLimit(): int {
+        return $this->limit;
+    }
+
     private function queryString(int $o): string {
         $params = [];
+        if ($this->sortBy !== null) {
+            $params[] = "sortBy={$this->sortBy}";
+        }
         if ($o > 0) {
             $params[] = "offset={$o}";
         }
@@ -40,32 +59,39 @@ class UserIndex {
             : $dataCount - $this->limit;
     }
 
+    public function getUrl() {
+        $path = explode('?', $_SERVER['REQUEST_URI'])[0];
+        return "http://{$_SERVER['HTTP_HOST']}{$path}";
+    }
+
     public function getLinks(): array {
-        $uri = '/api/users';
-        $links = [
-            'self' => [
-                'href' => $uri . $this->queryString($this->offset, $this->limit)
-            ],
-        ];
-        if ($this->offset > 0) {
-            $links['prev'] = [
-                'href' => $uri . $this->queryString($this->offset - $this->limit, $this->limit),
-            ];
-        }
+        $url = $this->getUrl();
         $next = $this->offset + $this->limit;
         $last = $this->getLastOffset();
+        $links = [];
+
+        if ($last > 0) {
+            $links['first'] = [
+                'href' => $url . $this->queryString(0),
+        ];
+        }
+        if ($this->offset > 0) {
+            $links['prev'] = [
+                'href' => $url . $this->queryString($this->offset - $this->limit, $this->limit),
+            ];
+        }
+        $links['self'] = [
+            'href' => $url . $this->queryString($this->offset, $this->limit),
+        ];
         if ($next <= $last) {
             $links['next'] = [
-                'href' => $uri . $this->queryString($next),
+                'href' => $url . $this->queryString($next),
             ];
         }
 
         if ($last > 0) {
-            $links['first'] = [
-                'href' => $uri . $this->queryString(0),
-            ];
             $links['last'] = [
-                'href' => $uri . $this->queryString($last),
+                'href' => $url . $this->queryString($last),
             ];
         }
         return $links;
